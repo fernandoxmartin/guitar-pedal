16dcf951d5dfbc359fa74d2e33551eb2c4e97302
fernandoxmartin <fernando@DESKTOP-ES6V7UH.localdomain>
2020-05-25 14:41:52 -0700

    FAQ accordion questions added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "../Styles/Home.scss";
 import guit1 from "../Images/guitars/00.webp";
-import guit2 from "../Images/guitars/03.webp";
-import guit3 from "../Images/guitars/07.webp";
+import guit2 from "../Images/guitars/07.webp";
+import guit3 from "../Images/guitars/08.webp";
 import guit4 from "../Images/guitars/09.webp";
+import ped1 from "../Images/pedals/01.webp";
+import ped2 from "../Images/pedals/02.webp";
+import ped3 from "../Images/pedals/05.webp";
+import ped4 from "../Images/pedals/03.webp";
 import gp from "../Images/logo-white.png";
 
 class Home extends Component {
@@ -20,7 +24,7 @@ class Home extends Component {
               <div className="promo-card">
                 <div className="promo-info">
                   <h4>
-                    Bundle Any Guitar | Pedal and <span>Save 30%</span>
+                    Bundle Any Guitar | Pedal Combo and <span>Save 30%</span>
                   </h4>
                 </div>
               </div>
@@ -29,12 +33,23 @@ class Home extends Component {
         </div>
         <div className="top-products">
           <div className="top-products-container">
-            <h2>Top Products</h2>
+            <h2>Top Guitars</h2>
             <div className="products">
               <img src={guit1} alt="guitar" />
               <img src={guit2} alt="guitar" />
               <img src={guit3} alt="guitar" />
               <img src={guit4} alt="guitar" />
+            </div>
+          </div>
+        </div>
+        <div className="top-products">
+          <div className="top-products-container">
+            <h2>Top Pedals</h2>
+            <div className="products">
+              <img src={ped1} alt="pedal" />
+              <img src={ped2} alt="pedal" />
+              <img src={ped3} alt="pedal" />
+              <img src={ped4} alt="pedal" />
             </div>
           </div>
         </div>
--- src/Components/Support.js
+++ src/Components/Support.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import "../Styles/Support.scss";
 import support from "../Images/support.png";
+import { FAQ } from "./FAQ";
 
 class Support extends Component {
+  reveal = (e) => {
+    e.parentElement.nextSibling.classList.toggle("answer-open");
+  };
+
   render() {
+    const questions = FAQ.map((question) => {
+      return (
+        <>
+          <div className="question">
+            <h4>{question.question}</h4>
+            <button className="arrow" onClick={(e) => this.reveal(e.target)} />
+          </div>
+          <div className="answer">
+            <p>{question.answer}</p>
+          </div>
+        </>
+      );
+    });
     return (
       <div className="support-container">
         <div className="support-banner">
@@ -12,6 +30,7 @@ class Support extends Component {
         </div>
         <div className="faq">
           <h2>FAQ</h2>
+          {questions}
         </div>
       </div>
     );
